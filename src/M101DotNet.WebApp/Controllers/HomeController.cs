d4bd91f0b79d8215c5dd2d8920d102c0970e941a
Docon <diane.oconnel@gmail.com>
2015-06-15 14:36:03 -0400

    Committing initial set up, and copied old completed homework code into the Home Controller for reference (commented out)

--- src/M101DotNet.WebApp/Controllers/HomeController.cs
+++ src/M101DotNet.WebApp/Controllers/HomeController.cs
@@ -29,6 +29,17 @@ namespace M101DotNet.WebApp.Controllers
             return View(model);
         }
 
+		//Homework 2.2
+		//[HttpGet]
+		//[AsyncTimeout(8000)]
+		//[HandleError(ExceptionType = typeof(TimeoutException), View = "TimedOut")]
+		//public async Task<ActionResult> Index()
+		//{
+		//	List<Grade> gradesDeleted = await DeleteGradesAsync();
+
+		//	return View(gradesDeleted);
+		//}
+
         [HttpGet]
         public ActionResult NewPost()
         {
@@ -98,5 +109,48 @@ namespace M101DotNet.WebApp.Controllers
 
             return RedirectToAction("Post", new { id = model.PostId });
         }
+
+		static async Task<List<Grade>> DeleteGradesAsync()
+	    {
+			var connectionString = "mongodb://localhost:27017";
+			var client = new MongoClient(connectionString);
+
+			//remove the grade of type "homework" with the lowest score for each student from the dataset
+
+			var db = client.GetDatabase("students");
+
+			var col = db.GetCollection<Grade>("grades");
+
+			//Hint: If you select homework grade-documents, sort by student and then by score, 
+			//you can iterate through and find the lowest score for each student by noticing 
+			//a change in student id. As you notice that change of student_id, remove the document.
+
+			var builder = Builders<Grade>.Filter;
+			var filter = builder.Eq(g => g.type,"homework");
+
+			var list = await col.Find(filter).Sort(Builders<Grade>.Sort.Ascending(g => g.student_id).Ascending(g => g.score)).ToListAsync();
+
+		    int previousStudentId = int.MinValue;
+			List<Grade> gradesToDelete = new List<Grade>();
+			foreach (var grade in list)
+			{
+				if (grade.student_id != previousStudentId)
+				{
+					gradesToDelete.Add(grade);
+					previousStudentId = grade.student_id;
+				}
+			}
+
+			List<Grade> gradesDeleted = new List<Grade>();
+			for(var i = 0; i < gradesToDelete.Count; i++)
+			{
+				var iterator = i;
+				gradesDeleted.Add(await col.FindOneAndDeleteAsync<Grade>(g => g.Id.Equals(gradesToDelete[iterator].Id), new FindOneAndDeleteOptions<Grade, Grade>{ MaxTime = TimeSpan.FromSeconds(60)}) ); 
+			}
+			//var result = await col.DeleteManyAsync(g => gradesToDelete.Select(s => s.Id).Contains(g.Id));
+
+		    return gradesDeleted;
+	    }
+    }
     }
 }
---
+++ src/M101DotNet.WebApp/Models/Grade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace M101DotNet.WebApp.Models
+{
+	public class Grade
+	{
+		public class Grade
+		{
+			public ObjectId Id { get; set; }
+			public int student_id { get; set; }
+			public double score { get; set; }
+			public string type { get; set; }
+		}
+	}
+}
